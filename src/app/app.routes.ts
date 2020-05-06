e3bba986e18ac35b30d42baf5567383651ba069f
Christian <christian@ascended.co.za>
2020-05-06 16:53:53 +0200

    Re-add page not found route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router'
 import { DashboardComponent } from './framework/components/dashboard/dashboard.component'
+import { PageNotFoundComponent } from './framework/components/page-not-found/page-not-found.component'
 
 export const routes: Routes = [
   {
     path: '',
     redirectTo: 'dashboard',
     pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
   },
   {
     path: 'dashboard',
